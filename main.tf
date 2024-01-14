6f69a23d0fac5891b456764ec553d59ea4c5c322
Paul Thrasher <thrashr888@gmail.com>
2024-01-13 19:26:51 -0800

    Emptying for now

--- main.tf
+++ main.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "3.2.2"
-    }
-  }
-}
-
-provider "null" {
-  # Configuration options
-}
-
-resource "null_resource" "print_junk" {
-  provisioner "local-exec" {
-    command = "find /tmp/"
-  }
-}
 
