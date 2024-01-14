a97d51f7b4ffe0520425beebfe85a60863643cac
Paul Thrasher <thrashr888@gmail.com>
2024-01-13 19:27:42 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.2.2"
+    }
+  }
+}
 
+provider "null" {
+  # Configuration options
+}
+
+resource "null_resource" "print_junk" {
+  provisioner "local-exec" {
+    command = "find /tmp/"
+  }
+}
